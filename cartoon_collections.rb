e5ea1e7ca93eaaef6358aeef223a58c1417f09e0
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2019-09-20 19:06:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,9 @@ def roll_call_dwarves(arr)
   arr.each_with_index {|name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element|
+    
 end
 
 def long_planeteer_calls# code an argument here
